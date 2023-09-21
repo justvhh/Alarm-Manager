2c13c6504d0ae199b0a94b86f9ac73c1ba3137e8
Hoang <hyangwu1608@gmail.com>
2023-09-21 21:52:06 +0700

    add alarm fragment

--- app/src/main/java/com/example/pj1/MainActivity.kt
+++ app/src/main/java/com/example/pj1/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.pj1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -10,17 +10,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.botton_nav)
-        val homeFragment = HomeFragment()
-        val noficationFragment = NoficationFragment()
-        val settingFragment = SettingFragment()
-        supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, homeFragment).commit()
-
-        bottomNavigationView.setOnItemReselectedListener {item ->
+        supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, HomeFragment()).commit()
+        bottomNavigationView.setOnItemSelectedListener {item ->
             when(item.itemId) {
-                R.id.nav_home -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, homeFragment).commit()
-                R.id.nav_notification -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, noficationFragment).commit()
-                R.id.nav_setting -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, settingFragment).commit()
+                R.id.nav_user -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, UserFragment()).commit()
+                R.id.nav_home -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, HomeFragment()).commit()
+                R.id.nav_notification -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, NotificationFragment()).commit()
+                R.id.nav_alarm -> supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, AlarmFragment()).commit()
             }
+            true
         }
     }
 }
